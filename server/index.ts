566ddbaf0fcdddd1c2153075b25eaebb82f157f2
yeshi wangdi <wangdzi@skywalker.fios-router.home>
2020-05-31 17:37:04 -0400

    added the Organ middleware

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,16 @@
-import { Application, send } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import { organ } from "https://raw.githubusercontent.com/denjucks/organ/master/mod.ts";
 import router from "./api/index.ts";
 const port = 5000;
 
 const app = new Application();
 
+//Organ is a logging middleware for the Oak web framework. It is based on the Morgan library for Express in NodeJS.
+app.use(organ());
+
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.use(async (context) => {
-  console.log(context)
-})
+
 
 console.log(`server running on port ${port}`);
 
